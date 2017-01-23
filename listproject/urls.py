9b132013bd46ddcb815c2551448c858f3f0555ff
Gaurav <oneknight2dark@gmail.com>
2017-01-24 00:47:18 +0530

    Added URL Redirect

--- listproject/urls.py
+++ listproject/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     # Examples:
@@ -9,5 +10,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^listapp/', include('listapp.urls')),
+    url(r'^$', RedirectView.as_view(url='listapp/')),
     url( r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }),
 ]
